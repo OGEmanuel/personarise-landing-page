5444b3b5f150542029e1f9147ddce760d0b94121
OGEmanuel <1blacksanta416@gmail.com>
2023-03-08 20:14:53 +0100

    hero page complete

--- components/hero-section.jsx
+++ components/hero-section.jsx
@@ -8,32 +8,45 @@ import star from '../public/images/star.svg';
 
 const HeroSection = () => {
   return (
-    <section>
-      <div>
-        <h1>Choose a career in tech in minutes</h1>
-        <p>
+    <section className="grid stack w-[80%] mx-auto my-20">
+      <Image
+        src={background}
+        alt="background"
+        className="grid-area-stack w-full"
+        priority
+      />
+      <div className="grid-area-stack w-[75%] mx-auto">
+        <h1 className="font-bold text-[64px] text-center mt-10 mb-5">
+          Choose a career in tech in minutes
+        </h1>
+        <p className="text-lg text-center w-[93%] mx-auto mb-4">
           Personarise is your tech careeer development platform for eliminating
           time consuming activities while making decision on a tech career you
           will succeed in — and so much more.
         </p>
-        <div>
-          <div>
-            <Image src={avy1} alt="avatar" />
-            <Image src={avy2} alt="avatar" />
-            <Image src={avy3} alt="avatar" />
-            <Image src={avy4} alt="avatar" />
+        <div className="flex w-max mx-auto gap-4 mb-10 items-center">
+          <div className="flex">
+            <Image src={avy1} alt="avatar" className="z-40" />
+            <Image src={avy2} alt="avatar" className="ml-[-1rem] z-30" />
+            <Image src={avy3} alt="avatar" className="ml-[-1rem] z-20" />
+            <Image src={avy4} alt="avatar" className="ml-[-1rem] z-10" />
           </div>
           <div>
-            <p>3k+ users</p>
+            <p className="text-sm">3k+ users</p>
             <Image src={star} alt="star" />
           </div>
         </div>
-        <form>
-          <input type="email" placeholder="Type your email address" />
-          <button>Get started</button>
+        <form className="w-[70%] mx-auto relative">
+          <input
+            type="email"
+            placeholder="Type your email address"
+            className="text-lg w-full py-2.5 pl-6 rounded-[12px] outline-none"
+          />
+          <button className="text-lg font-medium bg-secondary text-white py-1.5 px-3 rounded-[8px] absolute top-[8%] right-[.7%]">
+            Get started
+          </button>
         </form>
       </div>
-      <Image src={background} alt="background" />
     </section>
   );
 };
